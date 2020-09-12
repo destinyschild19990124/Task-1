bf5231194d9de72988f6870d84564c5b3b88c3cf
themu33inman <71170982+themu33inman@users.noreply.github.com>
2020-09-12 13:39:22 +0200

    new updates

---
+++ Task1/Goblin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Goblin : Enemy
+    {
+
+        public Goblin(int x,int y) : base(x, y, Tile.TileType.Enemy, 1, 10) { }
+
+        public override Movement returnMove()
+        {
+            Character.Movement direction = (Character.Movement)(rnd.Next(0, 6));
+
+            if(direction == Character.Movement.Up)
+            {
+
+            }else if(direction == Character.Movement.Down)
+            {
+
+            }else if(direction == Character.Movement.Left)
+            {
+
+            }else if(direction == Character.Movement.Right)
+            {
+
+            }
+            else
+            {
+                return Character.Movement.None;
+            }
+            
+        }
+
+
+    }
+}
---
+++ Task1/Hero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Hero : Character
+    {
+
+        public Hero(int x, int y,int hp) : base(x, y, Tile.TileType.Hero) {
+            this.setHp(hp);
+            this.setMaxHp(hp);
+            this.setDamage(2);
+        }
+
+
+    }
+}
